08b5c41c4e756f85927889420b2f6f23927d6ae7
luis <yongchun1683@gmail.com>
2019-04-06 16:56:10 +0800

    update python sandbox

--- python/python_sandbox/conditionals.py
+++ python/python_sandbox/conditionals.py
@@ -1,18 +1,47 @@
 # If/ Else conditions are used to decide to do something based on something being true or false
 
-
+num1 = 50
+num2 = 200
 
 # Comparison Operators (==, !=, >, <, >=, <=) - Used to compare values
 
+if num1>num2:
+  print(f'{num1} is greater than {num2}')
+else:
+  print(f'{num2} is greater than {num1}')
 
+
+a = 5
+b = 5
+
+if a < b:
+  print(f'{a} is less than {b}')
+elif a == b:
+  print(f'{a} is equal to {b}')
+else:
+  print(f'{a} is greater than {b}')
 
 # Logical operators (and, or, not) - Used to combine conditional statements
 
+x = 14
+
+if x<=10 and x>=5:
+  print(f'{x} is greater than 5 and less or equal to 10')
+else:
+  print('Something goes wrong')
 
 
 
 # Membership Operators (not, not in) - Membership operators are used to test if a sequence is presented in an object
 
+id = 100
+
+numbers = [10, 20, 30, 40]
+
+if id in numbers:
+  print(id in numbers)
+else:
+  print(f'{id} is no in the list')
 
 
 
--- python/python_sandbox/loops.py
+++ python/python_sandbox/loops.py
@@ -1,5 +1,30 @@
 # A for loop is used for iterating over a sequence (that is either a list, a tuple, a dictionary, a set, or a string).
 
+students = ['John', 'Paul', 'Daniel', 'Smith']
 
+for student in students:
+  print(student)
+
+fruits = ['Apple', 'Orange', 'Banana']
+
+for fruit in fruits:
+  print(fruit)
+  if fruit == 'Orange':
+    print(f'We break the loop because of we found {fruit}')
+    break
+
+# range
+
+for counter in range(len(students)):
+  print(students[counter])
+
+for i in range(0, 11):
+  print(f'Numbers: {i}')
 
 # While loops execute a set of statements as long as a condition is true.
+
+count = 0
+
+while count<10:
+  print(count)
+  count+=1
