5a09e838ddc83cecc25b37c47d4e73b29b8decd6
luis <yongchun1683@gmail.com>
2019-05-12 20:24:30 +0800

    add react tutorial from codevolution

---
+++ React/hello-world/src/components/Greet.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+// function Greet() {
+//   return <h1>Hello Matthew</h1>
+// }
+
+//! arrow function
+
+const Greet  = () => {
+  return <h1>Hello Matthew</h1>
+}
+
+export default Greet;
